dfc2c57957605d461b1f37e13094775aa6ea8059
mahmoud abdo <mah.abdo571@gmail.com>
2024-11-18 13:17:40 +0200

    Implement Browser Back Button

---
+++ HW/Implement Browser Back Button/Implement Browser Back Button/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebPage page = new WebPage();
+
+            while (true)
+            {
+                Console.Write("Enter url : ");
+                page.Url = Console.ReadLine();
+                page.currntPage();
+                page.openNewPage();
+
+                Console.Write("1 - back \n 2- close \n 3 - clos :");
+                if (int.TryParse(Console.ReadLine(), out int x))
+                {
+
+                    if (x == 1)
+                    {
+
+                        page.Back();
+                        page.currntPage();
+
+                    }
+
+                }
+                else if(x == 2)
+                {
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ HW/Implement Browser Back Button/Implement Browser Back Button/WebPage.cs
@@ -0,0 +1,40 @@
+﻿namespace MyApp
+{
+    public class WebPage
+    {
+
+        Stack<string> browser = new Stack<string>();
+
+        public  string Url { get; set; }
+
+        public WebPage()
+        {
+            Url = "";
+        }
+
+        public void Back()
+        {
+            browser.Pop();
+            Url = browser.Peek();
+
+
+        }
+
+        public void openNewPage()
+        {
+
+            browser.Push(Url);
+
+ 
+
+        }  
+        
+        public void currntPage()
+        {
+
+
+            Console.WriteLine($"currnt page {Url}");
+
+        }
+    }
+}
