40259918bdc01f26766f6f0bcc09a19f6093e71f
mahmoud abdo <mah.abdo571@gmail.com>
2024-11-19 14:15:53 +0200

    test

---
+++ HW/Convert Decimal to Binary/Convert Decimal to Binary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> pinaryDigit = new Stack<int>();
+
+            Console.Write("Enter Nuber To convert: ");
+            if(int.TryParse(Console.ReadLine(),out int x))
+            {
+                int tempint = x;
+
+                while (tempint != 0)
+                {
+                    pinaryDigit.Push(tempint % 2);
+
+                    tempint /= 2;
+
+                }
+            }
+
+            while(pinaryDigit.Count != 0)
+            {
+                Console.Write($"{pinaryDigit.Peek()}");
+                pinaryDigit.Pop();
+          
+            }
+
+           
+        }
+    }
+}
