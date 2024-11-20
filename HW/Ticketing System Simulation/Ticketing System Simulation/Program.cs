3313e8e9ce977d7353a7358a4e0d4396db8cd04a
mahmoud abdo <mah.abdo571@gmail.com>
2024-11-20 22:34:33 +0200

    Ticketing System Simulation

---
+++ HW/Ticketing System Simulation/Ticketing System Simulation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Sockets;
+
+namespace Ticketing_System_Simulation
+{
+  
+    internal class Program
+    {
+        static void _newTicket(ref Queue<int> ticket)
+        {
+         
+            int x = ticket.Last();
+            x++;
+            ticket.Enqueue(x);
+            Console.WriteLine($"Ticket {ticket.Last()} issued.");
+
+     
+
+        }
+        static void Main(string[] args)
+        {
+            Queue<int> tickets = new Queue<int>();
+
+            tickets.Enqueue(100);
+            if (tickets.Count > 0) {
+                _newTicket(ref tickets);
+                _newTicket(ref tickets);
+                _newTicket(ref tickets);
+                _newTicket(ref tickets);
+                _newTicket(ref tickets);
+            }
+            Console.WriteLine("Ticketing System Simulation Started...\r\n\r\n");
+            while(tickets.Count > 0)
+            {
+                Thread.Sleep(500);
+                var ticket = tickets.Dequeue();
+                Console.WriteLine($"Processing Ticket: {ticket}");
+                //Remaining Tickets: 102, 103, 104, 105
+
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(500);
+                    Console.Write("*");
+                    if(i == 4)
+                    {
+                        Console.WriteLine();
+                    }
+           
+                }
+                Console.WriteLine($"Remaining Tickets: {string.Join(",",tickets)}");
+                Console.WriteLine("-----");
+
+
+            }
+            Console.WriteLine("No more tickets in the queue.\r\n\r\nTicketing System Simulation Ended.");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
--- HW/Traffic Signal System Simulation/Traffic Signal System Simulation/Program.cs
+++ HW/Traffic Signal System Simulation/Traffic Signal System Simulation/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Traffic_Signal_System_Simulation
+﻿using System.Diagnostics;
+
+namespace Traffic_Signal_System_Simulation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Queue<string> trafficQueue = new Queue<string>();
+
+
+     
+            trafficQueue.Enqueue("Car 1");
+            trafficQueue.Enqueue("Truck 1");
+            trafficQueue.Enqueue("Bike 1");
+            trafficQueue.Enqueue("Bus 1");
+
+
+            Console.WriteLine("Traffic Signal Simulation Started...\n");
+
+            while (trafficQueue.Count > 0)
+            {
+               
+                string vehicle = trafficQueue.Dequeue();
+                Console.WriteLine($"{vehicle} has passed the signal.\n");
+
+
+              
+                if (trafficQueue.Count > 0)
+                {
+                    Console.WriteLine("Vehicles waiting: " + string.Join(", ", trafficQueue));
+                }
+                else
+                {
+                    Console.WriteLine("No vehicles waiting.");
+                }
+
+
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("Traffic Signal Simulation Ended.");
+            Console.ReadKey();
+
+
         }
     }
 }
