e3cc3b6b9ce3a4fcef953ca5ad1f67b56e37ced3
mahmoud abdo <mah.abdo571@gmail.com>
2024-11-20 23:03:18 +0200

    Implementing_Simple_Backtracking

---
+++ HW/Implementing Simple Backtracking/Implementing Simple Backtracking/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace Implementing_Simple_Backtracking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> Backtracking = new Stack<string>();
+            //Start -> Go to Gaz Station -> Go to Super Market -> Go To Work -> Go to Cafe -> Go Home.
+
+           // Backtracking...
+            Backtracking.Push("Start");
+
+            Backtracking.Push("Gaz Station");
+
+            Backtracking.Push("Super Market");
+           
+            Backtracking.Push("Work");
+  
+            Backtracking.Push("Cafe");
+     
+            Backtracking.Push("Home");
+            Console.WriteLine("Start -> Go to Gaz Station -> Go to Super Market -> Go To Work -> Go to Cafe -> Go Home.");
+
+            while (Backtracking.Count > 0) {
+
+                Console.WriteLine($"Back to: {Backtracking.Pop()}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
